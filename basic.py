6173b3eb3ce99cdf245c51288ea9e89aa8c8a8d6
Anisur Rahman <ranisur056@gmail.com>
2022-06-06 16:18:43 +0600

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,16 +4,16 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 def window():
-app = QApplication(sys.argv)
-w = QWidget()
-w.setWindowTitle("Anis Tech LTD")
-w.setGeometry(300,200,800,400)
+   app = QApplication(sys.argv)
+   w = QWidget()
+   w.setWindowTitle("Anis Tech LTD")
+   w.setGeometry(300,200,800,400)
 
-b = QLabel(w)
-b.setText("Welcome to Anisur Rahman Tech LTD")
+   b = QLabel(w)
+   b.setText("Welcome to Anisur Rahman Tech LTD")
 
-w.show()
-sys.exit(app.exec_())
+   w.show()
+   sys.exit(app.exec_())
 
-if name == 'main':
-window()
+if __name__ == '__main__':
+   window()
